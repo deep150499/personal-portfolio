b44019d6e87003a429a4b49f450ea0baac4c1946
“deep150499” <deepnanavati@icloud.com>
2024-03-21 18:10:06 -0400

    image

--- app/layout.js
+++ app/layout.js
@@ -6,6 +6,8 @@ import Footer from "@/app/Footer/page";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+      </head>
       <body>
         <Theming>
           <Header />
