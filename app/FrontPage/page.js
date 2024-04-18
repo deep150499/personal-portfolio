6f87b2fdc221563a384e5a5bebe0091e3e34661c
“deep150499” <deepnanavati@icloud.com>
2024-04-17 20:23:41 -0400

    resume

--- app/FrontPage/page.js
+++ app/FrontPage/page.js
@@ -40,7 +40,7 @@ function Frpage() {
         As a Front End Web Developer, my goal is to utilize my proficiency in HTML, CSS, Tailwind CSS, Bootstrap, JavaScript, Typescript, React Js, Next Js to create visually stunning and user-friendly websites. With a keen eye for design and a passion for coding, I am dedicated to delivering innovative solutions that enhance the digital presence of businesses and provide exceptional user experiences.
         </p>
 
-        <Link href={'https://drive.google.com/file/d/1CE4nnG7lpM7kAugYAX0f8G4DoYHlIv2w/view?usp=sharing'} target="_blank"><button className="mt-10 border border-neutral-700/30 rounded-md p-2 w-full lg:w-44 bg-neutral-800/20 text-sm dark:text-neutral-400 font-semibold">
+        <Link href={'https://drive.google.com/file/d/1nnVZSTg6cKCAR_SxCquwZz0-5t4nbFTN/view?usp=sharing'} target="_blank"><button className="mt-10 border border-neutral-700/30 rounded-md p-2 w-full lg:w-44 bg-neutral-800/20 text-sm dark:text-neutral-400 font-semibold">
           View resume
         </button>
         </Link>
